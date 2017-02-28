8ae7d616342be8717c190c438014ca2f4646ba8c
Peter O'Shaughnessy <peter.oshaughnessy@gmail.com>
2017-02-28 13:13:33 +0000

    Trying to fix sw references for deployment

--- sw.js
+++ sw.js
@@ -2,11 +2,11 @@
 
 importScripts('lib/sw-toolbox.js');
 
-toolbox.precache(['/']);
+toolbox.precache(['index.html']);
 
 // TODO precache images? Or cache on fetch?
-toolbox.router.get('/images/*', toolbox.cacheFirst);
+toolbox.router.get('images/*', toolbox.cacheFirst);
 
-toolbox.router.get('/*', toolbox.networkFirst, {
+toolbox.router.get('*', toolbox.networkFirst, {
   networkTimeoutSeconds: 2
 });
